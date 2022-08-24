5049ed62926f63ebe85eca0125891e8a53c2f31a
Tim Turvey <tturvey@hotmail.co.uk>
2022-08-24 10:44:44 +0100

    Refactor: integer value of string

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,13 +1,8 @@
 public class StringCalculator {
 
     public Integer add(String str) {
-        if (str.equals("1")) {
-            return 1;
-        }
-        if (str.equals("2")) {
-            return 2;
-        }
-        return 3;
+
+        return Integer.valueOf(str);
     }
 
 
