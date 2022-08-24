920565a65b999a0cae84cc9b5792d83a595cf60d
Tim Turvey <tturvey@hotmail.co.uk>
2022-08-24 10:35:07 +0100

    Red: 1 fails

---
+++ src/test/java/StringCalculatorTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StringCalculatorTest {
+
+    @Test
+    public void
+    given_decimal_returns_roman_numeral_string() {
+        assertEquals(1, new StringCalculator().add(1));
+    }
+}
